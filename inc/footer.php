0865d87134cf86b6ebb4fee0894b9d6c681d6fe7
revxllor <rolliverhernandez0@gmail.com>
2024-11-28 22:51:10 +0800

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<!-- footer.php -->
+<footer>
+    <div class="footer-content">
+        <!-- Logo -->
+        <div class="footer-logo">
+            <img src="img/user.jpg" alt="TimeWise Logo">
+        </div>
+
+        <!-- Copyright Text -->
+        <div class="footer-text">
+            <p>&copy; 2024 TimeWise. All rights reserved. Time Management System</p>
+        </div>
+    </div>
+</footer>
